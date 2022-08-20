46b3c72aab849c2440278899e455a714d42b8cde
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-20 22:16:24 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { CarouselProps } from "../data/CarouselProps";
 
 const NewSlider: React.FC = () => {
