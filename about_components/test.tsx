8225915a87258681045283d2b82693158b79174e
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-20 22:34:52 +0530

    Add files via upload

---
+++ about_components/test.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+import React from "react";
+import { MessageDataType } from "../types";
+
+const test = () => {
+   return(
+    <p>This is about page</p>
+   )
+};
