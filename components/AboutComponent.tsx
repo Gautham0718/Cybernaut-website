d639380dbbe3ded2699e0d206c0e5559ed3df1c6
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-21 14:37:18 +0530

    Update AboutComponent.tsx

--- components/AboutComponent.tsx
+++ components/AboutComponent.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { aboutPageChildren } from "../constants/AboutData";
 import StatusBar from "./StatusBar";
+import HomeLogo from "../public/HomeLogo.jpeg";
 
 const AboutComponent = () => {
   const [currTopic, setCurrTopic] = useState(0);
@@ -9,7 +10,9 @@ const AboutComponent = () => {
       id="about"
       className="mt-16 w-full h-fit lg:h-[600px] md:w-4/5 flex mb-10"
     >
-      <div className=" hidden lg:flex lg:w-1/3 h-full bg-gray-200 rounded-md p-16"></div>
+      <div className=" hidden lg:flex lg:w-1/3 h-full bg-gray-200 rounded-md p-16"> 
+        <img className="flex float-left absolute" src = {HomeLogo.src}/>
+      </div>
       <div className="w-full sm:w-full lg:w-2/3 h-full flex items-center justify-start p-6 py-2">
         <StatusBar
           onPress={(val: number) => {
