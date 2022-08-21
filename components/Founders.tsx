e22c8b7ba2515aaad5caa8e976fcc80d6e3629b0
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-22 00:02:35 +0530

    Update Founders.tsx

--- components/Founders.tsx
+++ components/Founders.tsx
@@ -46,7 +46,7 @@ const Founders = () => {
         </div>
       </Carousel>
       
-      <div className="py-10 w-[20%] h-6 flex items-center justify-between bg-white">
+      <div className="py-10 w-[20%] h-6 flex items-center justify-between">
         <div
           className={
             selectedSlide === 0
