6d53bf563f19d061584755eb8becf7e1111541f3
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-22 21:31:42 +0530

    Update Founders.tsx

--- components/Founders.tsx
+++ components/Founders.tsx
@@ -20,25 +20,25 @@ const Founders = () => {
         }}
         {...(CarouselProps as any)}
       >
-        <div className="lg:h-[70%] ">
+        <div className="flex flex-col items-center justify-center gap-y-4 lg:h-[70%] ">
         
-        <div className="bg-gray-200 p-4 flex flex-col w-full items-center justify-between">
+        <div className="bg-gray-200 p-4 flex flex-col items-center justify-between">
         <img className="h-full" src={IMG.src} />
           <p className="font-semibold text-xl">JAYASURYA</p>
           <p>Founder & Chief Executive Officer</p>
         </div>
       </div>
-      <div className=" lg:h-[70%] ">
+      <div className="flex flex-col items-center justify-center gap-y-4 lg:h-[70%] ">
         
-        <div className="bg-gray-200 p-4 flex flex-col w-full items-center justify-between">
+        <div className="bg-gray-200 p-4 flex flex-col items-center justify-between">
         <img className="h-full" src={IMG.src} />
           <p className="font-semibold text-xl">B MANISH KUMAR</p>
           <p>Co-Founder & President</p>
         </div>
       </div>
-      <div className="lg:h-[70%] ">
+      <div className="flex flex-col items-center justify-center gap-y-4 lg:h-[70%] ">
         
-        <div className="bg-gray-200 p-4 flex flex-col w-full items-center justify-between">
+        <div className="bg-gray-200 p-4 flex flex-col  items-center justify-between">
         <img className="h-full" src={IMG.src} />
           <p className="font-semibold text-xl">R KEERTHANA</p>
           <p>Co-Founder & Vice-President</p>
