864d5fbf60e546cc6416e0cc87959757b12dee00
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-25 19:53:02 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -132,12 +132,12 @@ const Contacts: React.FC<Props> = ({ submitDataFN }) => {
       </div>
       <div className="h-full">
         <div className="flex flex-col">
-          <div className="flex-row">
-            <h4 className="text-6x1">Instagram @cybernaut_official</h4>
-            <div className="flex mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-blue-300"></div>
+          <div className="flex flex-row">
+            <h4 className="text-6x1 flex-row">Instagram @cybernaut_official</h4>
+            <div className="flex flex-row mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-blue-300"></div>
           </div>
             <div className="flex flex-row justify-start float-left mt-20  bg-blue-300">
-              <img className="h-[250px] w-[150px]" src={temp.src}/>
+              <img className="h-[500px] w-[150px]" src={temp.src}/>
               <img className="h-[250px] w-[150px]"  src={temp.src}/>
               <img className="h-[250px] w-[150px]"  src={temp.src}/>
               <img className="h-[250px] w-[150px]"  src={temp.src}/>
