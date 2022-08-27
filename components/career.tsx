b268f4e8abc7af1b61855b90992617af4880e4aa
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-27 20:27:27 +0530

    Update career.tsx

--- components/career.tsx
+++ components/career.tsx
@@ -9,7 +9,7 @@ const career = () => {
         <div className="flex lg:h-[50%] w-full"> 
             <img src={bg.src} />
         </div>
-        <div className="absolute flex h-[80%] w-[60%] justify-center align-middle items-center bg-white">
+        <div className="absolute flex h-[60%] w-[80%] justify-center align-middle items-center bg-white">
             <div className="flex flex-row mx-4 justify-between">
                 <div>
                     <div className="flex flex-row">
@@ -20,7 +20,7 @@ const career = () => {
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
                     <p className=" mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                     </div>
-                    <div className="gap-y-2 float-left ">
+                    <div className="gap-y-2 float-left mt-4">
                          <ViewCourseButton text="Explore the available career oppurtunities " />
                     </div>
                 </div>
