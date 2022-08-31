ddf5b6710a0b7899313abd8217962b32fc3e90bf
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-31 21:53:46 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -36,13 +36,13 @@ const NewSlider: React.FC = () => {
               <p className="justify-center align-middle items-center">content here</p>
             </div>
               </div>
-            <div className=" px-[500px]">
-            <div className="mt-32">
+            <div className=" px-[550px]">
+            <div className="mt-32 justify-evenly">
           <div className="flex items-center gap-x-4">
             <p className="text-2xl font-bold">TECH COURSES</p>
             <div className="w-14 h-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-blue-400"></div>
           </div>
-          <p className="p-4 justify-items-start">
+          <p className="p-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Reprehenderit tempore, ullam illum nam maxime porro sapiente
             assumenda harum tempora eos repellendus vitae provident a molestiae
