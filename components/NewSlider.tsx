2a1b869d87f622380790617afee3f3896dbf3e3b
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-31 23:17:40 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -36,7 +36,7 @@ const NewSlider: React.FC = () => {
               <p className="justify-center align-middle items-center">content here</p>
             </div>
               </div>
-            <div className=" px-[600px]">
+            <div className=" px-[500px]">
             <div className="mt-24 text-justify gap-y-4">
           <div className="flex items-center gap-x-4">
             <p className=" text-4xl font-bold">TECH COURSES</p>
