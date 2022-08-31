5a62629329cc093c54c25cb12d60d52761cf9f85
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-31 23:54:42 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -36,13 +36,13 @@ const NewSlider: React.FC = () => {
               <p className="justify-center align-middle items-center">content here</p>
             </div>
               </div>
-            <div className="w-[1000px] mr-40 px-[500px]">
+            <div className="mr-40 px-[500px]">
             <div className="mt-24 text-justify gap-y-4">
           <div className="flex items-center gap-x-4">
             <p className=" text-4xl font-bold">TECH COURSES</p>
             <div className="w-14 h-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-blue-400"></div>
           </div>
-          <p className="py-1">
+          <p className="py-1 w-96">
           There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
           </p>
           <div className="flex flex-row py-4 gap-x-4">
