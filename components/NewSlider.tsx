41616d2566742ff377a244ccd41526aa08b6010b
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-31 18:11:59 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -27,12 +27,12 @@ const NewSlider: React.FC = () => {
             
             <div className=" border-solid border border-gray-400 rounded-full h-96 w-96 origin-top-left mt-16">
               <AcademicCapIcon className="absolute py-0 w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-20"/>
-              <AtSymbolIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-40"/>
-              <BellIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-80 translate-y-20"/>
-              <BookmarkIcon className="w-10 h-10 absolute border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-80 translate-y-64"/>
-              <BriefcaseIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-40 translate-y-96 "/>
+              <AtSymbolIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-44 -translate-y-4"/>
+              <BellIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-96 translate-y-20"/>
+              <BookmarkIcon className="w-10 h-10 absolute border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-88 translate-y-64"/>
+              <BriefcaseIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-40 translate-y-80 "/>
               <LogoutIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-64"/>
-            <div className=" absolute m-24 origin-center justify-center align-middle items-center border border-solid border-white bg-white rounded-full h-40 w-40">
+            <div className=" absolute m-24 origin-center justify-center align-middle items-center border border-solid border-white bg-white rounded-full h-50 w-50">
               <p className="justify-center align-middle items-center">content here</p>
             </div>
               </div>
