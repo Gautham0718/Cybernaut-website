418323ff4cb501babb022ef7bc3cee33270c6eff
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-08-31 16:55:40 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -23,16 +23,16 @@ const NewSlider: React.FC = () => {
         </div>
         <div className="lg:h-[70%] bg-gray-200 ml-6 m-6">
           <div className="flex flex-row align-middle items-center ml-12">
+
             
-            <AcademicCapIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 "/>
+            <div className=" border-solid border border-gray-400 rounded-full h-96 w-96 origin-top-left mt-16">
+              <AcademicCapIcon className="absolute py-2 w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 "/>
               <AtSymbolIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 "/>
               <BellIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400"/>
               <BookmarkIcon className="w-10 h-10 absolute border border-solid border-gray-400 rounded-full bg-white text-blue-400 "/>
               <BriefcaseIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 "/>
               <LogoutIcon className="absolute w-10 h-10 border border-solid border-gray-400 rounded-full bg-white text-blue-400 "/>
-            <div className=" border-solid border border-gray-400 rounded-full h-96 w-96 origin-top-left mt-16">
-              
-            <div className=" absolute m-48 origin-center justify-center align-middle items-center border border-solid border-white bg-white rounded-full h-40 w-40">
+            <div className=" absolute m-36 origin-center justify-center align-middle items-center border border-solid border-white bg-white rounded-full h-40 w-40">
               <p className="justify-center align-middle items-center">content here</p>
             </div>
               </div>
