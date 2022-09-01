948af549a5a203873b5dc1c1b8d80f7c72d5952f
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-01 23:00:51 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -60,44 +60,11 @@ const NewSlider: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="lg:h-[70%] bg-gray-200  m-10">
-          <div className="flex flex-row align-middle items-center ml-20">
-
-            
-            <div className="absolute border-solid border border-gray-400  h-96 w-96 rounded-[500px] origin-top-left mt-[10%]">
-              <AcademicCapIcon className="absolute cursor-pointer py-0 w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-20"/>
-              <AtSymbolIcon className="absolute cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-44 -translate-y-4"/>
-              <BellIcon className="absolute cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-[340px] translate-y-20"/>
-              <BookmarkIcon className="w-12 h-12 absolute cursor-pointer border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-[340px] translate-y-64"/>
-              <BriefcaseIcon className="absolute cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-44 translate-y-[360px] "/>
-              <LogoutIcon className="absolute cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-64"/>
-            <div className=" absolute m-16 origin-center justify-center align-middle items-center border border-solid border-white bg-white w-64 h-64 rounded-full">
-              <p id="title" className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">TECH COURSE</p>
-              <p id="content" className="justify-center pt-2 align-middle items-center text-2x1"></p>
-            </div>
-              </div>
-            <div className=" pl-[500px] pr-4">
-            <div className="text-justify gap-y-4 mt-12">
-          <div className="flex items-center gap-x-4">
-            <p className=" text-4xl font-bold">TECH COURSES</p>
-            <div className="w-14 h-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-blue-400"></div>
-          </div>
-          <p className="py-1">
-          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
-          </p>
-          <div className="flex flex-row py-4 gap-x-4">
-            <p className="border w-28 h-9 text-center border-solid border-gray-400 rounded-2xl font-semibold ">30+ Hours</p>
-            <p className="border w-48 h-9 text-center border-solid border-gray-400 rounded-2xl font-semibold ">Access to study materials</p>
-          </div>
-          <div className="py-1">
-          <p className="border w-64 h-9 text-center border-solid border-gray-400 rounded-2xl font-semibold ">Basic to intermediate coaching</p>
-          </div>
-          <div className="py-1">
-          <p className="border w-24 h-9 border-solid text-center border-gray-400 rounded-2xl font-semibold">Certificate</p>
-          </div>
-        </div>
-            </div>
-          </div>
+        <div className="lg:h-[70%]">
+          <img
+            className="h-full"
+            src="https://www.cybernautofficial.com/static/media/cybernautPoster.f537893d.jpeg"
+          />
         </div>
       </Carousel>
       <div className="w-[20%] h-6 flex items-center justify-between bg-white">
