6a3e47bf5016cea4372711bdd6726d03642d4f17
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-03 17:13:42 +0530

    Add files via upload

---
+++ constants/TechCourses.tsx
@@ -0,0 +1,61 @@
+
+import React from "react";
+
+export const TechData = [
+  <div
+    key={"0"}
+  >
+    <div>
+    <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+    <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+    </div>
+    </div>,
+  <div
+  key={"1"}
+>
+  <div>
+  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+  </div>
+  </div>,
+  <div
+  key={"2"}
+>
+  <div>
+  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+  </div>
+  </div>,
+   <div
+   key={"3"}
+ >
+   <div>
+   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+   <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+   </div>
+   </div>, 
+   <div
+  key={"4"}
+>
+  <div>
+  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+  </div>
+  </div>,
+   <div
+   key={"5"}
+ >
+   <div>
+   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+   <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+   </div>
+   </div>,
+    <div
+    key={"6"}
+  >
+    <div>
+    <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
+    <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+    </div>
+    </div>,
+];
