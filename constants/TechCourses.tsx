ea1162802c0be711d4353a29d5930c7abb6e3f84
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-03 17:46:37 +0530

    Update TechCourses.tsx

--- constants/TechCourses.tsx
+++ constants/TechCourses.tsx
@@ -6,56 +6,56 @@ export const TechData = [
     key={"0"}
   >
     <div>
-    <p className=" justify-center pt-[20%] align-middle items-center font-bold text-4x1 ">CYBERNAUT</p>
-    <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 ">Welcome to cybernaut tech courses</p>
+    <p >CYBERNAUT</p>
+    <p className=" w-24 ">Welcome to cybernaut tech courses</p>
     </div>
     </div>,
   <div
   key={"1"}
 >
   <div>
-  <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 ">FIGMA</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1 w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p >FIGMA</p>
+  <p className=" w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
   <div
   key={"2"}
 >
   <div>
-  <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 ">C++</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p >C++</p>
+  <p className=" w-24 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"3"}
  >
    <div>
-   <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 ">PYTHON</p>
-   <p className="justify-center pt-2 align-middle items-center text-2x1 w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p >PYTHON</p>
+   <p className=" w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>, 
    <div
   key={"4"}
 >
   <div>
-  <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 ">MYSQL</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1 w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p >MYSQL</p>
+  <p className=" w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"5"}
  >
    <div>
-   <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 ">JAVA</p>
-   <p className="justify-center pt-2 align-middle items-center text-2x1 w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p >JAVA</p>
+   <p className=" w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>,
     <div
     key={"6"}
   >
     <div>
-    <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 ">HTML</p>
-    <p className="justify-center pt-2 align-middle items-center text-2x1 w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p >HTML</p>
+    <p className=" w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     </div>,
 ];
