a12faecab2379ac9db1ee9b1d84f68dffa230e97
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-03 17:26:04 +0530

    Update TechCourses.tsx

--- constants/TechCourses.tsx
+++ constants/TechCourses.tsx
@@ -6,56 +6,56 @@ export const TechData = [
     key={"0"}
   >
     <div>
-    <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
-    <p className="justify-center pt-2 align-middle items-center text-2x1 w-16 ">Welcome to cybernaut tech courses</p>
+    <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">CYBERNAUT</p>
+    <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6 ">Welcome to cybernaut tech courses</p>
     </div>
     </div>,
   <div
   key={"1"}
 >
   <div>
-  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">FIGMA</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">FIGMA</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
   <div
   key={"2"}
 >
   <div>
-  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">C++</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">C++</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"3"}
  >
    <div>
-   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">PYTHON</p>
-   <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">PYTHON</p>
+   <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>, 
    <div
   key={"4"}
 >
   <div>
-  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">MYSQL</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">MYSQL</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"5"}
  >
    <div>
-   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">JAVA</p>
-   <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">JAVA</p>
+   <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>,
     <div
     key={"6"}
   >
     <div>
-    <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">HTML</p>
-    <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p className=" justify-center pt-[20%] align-middle items-center font-bold text-2x1 w-24 pl-6">HTML</p>
+    <p className="justify-center pt-2 align-middle items-center text-2x1 w-24 pl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     </div>,
 ];
