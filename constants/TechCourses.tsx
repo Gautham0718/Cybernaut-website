ac5d330cc7dc3704e3183b96d8a97c172e6421c4
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-03 19:39:11 +0530

    Update TechCourses.tsx

--- constants/TechCourses.tsx
+++ constants/TechCourses.tsx
@@ -6,56 +6,56 @@ export const TechData = [
     key={"0"}
   >
     <div>
-    <p className="font-bold text-4x1">CYBERNAUT</p>
-    <p className=" justify-center txt-center w-32 ">Welcome to cybernaut tech courses</p>
+    <p className="font-bold text-4xl">CYBERNAUT</p>
+    <p className=" justify-center txt-center w-24 ">Welcome to cybernaut tech courses</p>
     </div>
     </div>,
   <div
   key={"1"}
 >
   <div>
-  <p className="font-bold text-4x1">FIGMA</p>
-  <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className="font-bold text-4xl">FIGMA</p>
+  <p className=" justify-center txt-center w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
   <div
   key={"2"}
 >
   <div>
-  <p className="font-bold text-4x1">C++</p>
-  <p className=" justify-center txt-center w-32 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className="font-bold text-4xl">C++</p>
+  <p className=" justify-center txt-center w-24 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"3"}
  >
    <div>
-   <p className="font-bold text-4x1">PYTHON</p>
-   <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p className="font-bold text-4xl">PYTHON</p>
+   <p className=" justify-center txt-center w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>, 
    <div
   key={"4"}
 >
   <div>
-  <p className="font-bold text-4x1" >MYSQL</p>
-  <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className="font-bold text-4xl" >MYSQL</p>
+  <p className=" justify-center txt-center w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"5"}
  >
    <div>
-   <p className="font-bold text-4x1">JAVA</p>
-   <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p className="font-bold text-4xl">JAVA</p>
+   <p className=" justify-center txt-center w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>,
     <div
     key={"6"}
   >
     <div>
-    <p className="font-bold text-4x1">HTML</p>
-    <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p className="font-bold text-4xl">HTML</p>
+    <p className=" justify-center txt-center w-24">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     </div>,
 ];
