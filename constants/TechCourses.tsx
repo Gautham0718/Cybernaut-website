210b7107a37c25b4035f7a4e5cc4708d035cfd3c
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-03 17:22:47 +0530

    Update TechCourses.tsx

--- constants/TechCourses.tsx
+++ constants/TechCourses.tsx
@@ -7,55 +7,55 @@ export const TechData = [
   >
     <div>
     <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">CYBERNAUT</p>
-    <p className="justify-center pt-2 align-middle items-center text-2x1">Welcome to cybernaut tech courses</p>
+    <p className="justify-center pt-2 align-middle items-center text-2x1 w-16 ">Welcome to cybernaut tech courses</p>
     </div>
     </div>,
   <div
   key={"1"}
 >
   <div>
-  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">FIGMA</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">FIGMA</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
   <div
   key={"2"}
 >
   <div>
-  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">C++</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">C++</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"3"}
  >
    <div>
-   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">PYTHON</p>
-   <p className="justify-center pt-2 align-middle items-center text-2x1">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">PYTHON</p>
+   <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>, 
    <div
   key={"4"}
 >
   <div>
-  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">MYSQL</p>
-  <p className="justify-center pt-2 align-middle items-center text-2x1">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">MYSQL</p>
+  <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
   </div>,
    <div
    key={"5"}
  >
    <div>
-   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">JAVA</p>
-   <p className="justify-center pt-2 align-middle items-center text-2x1">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">JAVA</p>
+   <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
    </div>,
     <div
     key={"6"}
   >
     <div>
-    <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1">HTML</p>
-    <p className="justify-center pt-2 align-middle items-center text-2x1">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p className=" justify-center pt-[25%] align-middle items-center font-bold text-2x1 w-16">HTML</p>
+    <p className="justify-center pt-2 align-middle items-center text-2x1 w-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     </div>,
 ];
