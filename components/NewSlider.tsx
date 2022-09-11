02e05cada5d8fb55e7a524fc3228ec041923955c
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-11 21:58:56 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -47,13 +47,13 @@ const NewSlider: React.FC = () => {
         }}
         {...(CarouselProps as any)}
       >
-        <div className="lg:h-full">
+        <div className="h-full">
           <img
             className="h-full"
             src={Logo.src}
           />
         </div>
-        <div className="lg:h-full bg-gray-200  m-10">
+        <div className="h-full bg-gray-200  m-10">
           <div className="flex flex-row align-middle items-center ml-20">
 
             
@@ -91,7 +91,7 @@ const NewSlider: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className=" bg-gray-200  m-10">
+        <div className="h-full bg-gray-200  m-10">
           <p>Helloworld</p>
         </div>
       </Carousel>
