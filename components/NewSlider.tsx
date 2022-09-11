6382a0fa0bff7f54b98dff4b266c540a87becc72
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-11 22:41:32 +0530

    Update NewSlider.tsx

--- components/NewSlider.tsx
+++ components/NewSlider.tsx
@@ -93,7 +93,7 @@ const NewSlider: React.FC = () => {
         </div>
         <div className="h-full bg-gray-200  m-10">
         <div className="flex flex-row align-middle items-center ml-20">
-        <div className=" pl-20 w-auto mr-[800px]">
+        <div className=" pl-20 w-full columns-[50%] pr-20">
             <div className="text-justify gap-y-4 mt-12">
           <div className="flex items-center gap-x-4">
             <p className=" text-4xl font-bold">TECH COURSES</p>
@@ -114,8 +114,7 @@ const NewSlider: React.FC = () => {
           </div>
         </div>
             </div>
-            <div className="pl-[600px]">
-            <div className="absolute border-solid border border-gray-400 h-96 w-96 rounded-[500px] origin-top-left mt-20">
+            <div className="absolute border-solid border border-gray-400 pl-[600px] h-96 w-96 rounded-[500px] origin-top-left mt-20">
               <AcademicCapIcon href="#" onClick={figma} className="absolute shadow-blue-200 shadow-sm ease-linear duration-200 hover:bg-blue-400 hover:text-white cursor-pointer py-0 w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-20"/>
               <AtSymbolIcon onClick={cpp} className="absolute shadow-blue-200  hover:bg-blue-400 hover:text-white cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-44 -translate-y-4"/>
               <BellIcon onClick={python} className="absolute shadow-blue-200  hover:bg-blue-400 hover:text-white cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-[340px] translate-y-20"/>
@@ -124,7 +123,6 @@ const NewSlider: React.FC = () => {
               <LogoutIcon onClick={html} className="absolute shadow-blue-200  hover:bg-blue-400 hover:text-white cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-64"/>
             <div className=" absolute m-16 origin-center ease-linear duration-200 justify-center align-middle items-center border border-solid border-white bg-white w-64 h-64 rounded-full">
               <p className=" justify-center ease-linear duration-200 pt-[25%] align-middle items-center text-2x1">{TechData[ind]}</p>
-            </div>
             </div>
             </div>
             </div>
