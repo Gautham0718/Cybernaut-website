6731bcd610fe24ddc169a79add7ff12b7753a7d4
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-14 22:52:56 +0530

    Update TechCourses.tsx

--- constants/TechCourses.tsx
+++ constants/TechCourses.tsx
@@ -58,4 +58,60 @@ export const TechData = [
     <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     </div>,
+    <div
+    key={"7"}
+  >
+    <div>
+    <p className="font-bold text-2xl">CYBERNAUT</p>
+    <p className=" justify-center txt-center w-32 ">Welcome to cybernaut tech courses</p>
+    </div>
+    </div>,
+  <div
+  key={"8"}
+>
+  <div>
+  <p className="font-bold text-2xl">FIGMA</p>
+  <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+  </div>,
+  <div
+  key={"9"}
+>
+  <div>
+  <p className="font-bold text-2xl">C++</p>
+  <p className=" justify-center txt-center w-32 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+  </div>,
+   <div
+   key={"10"}
+ >
+   <div>
+   <p className="font-bold text-2xl">PYTHON</p>
+   <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   </div>
+   </div>, 
+   <div
+  key={"11"}
+>
+  <div>
+  <p className="font-bold text-2xl" >MYSQL</p>
+  <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+  </div>,
+   <div
+   key={"12"}
+ >
+   <div>
+   <p className="font-bold text-2xl">JAVA</p>
+   <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   </div>
+   </div>,
+    <div
+    key={"6"}
+  >
+    <div>
+    <p className="font-bold text-2xl">HTML</p>
+    <p className=" justify-center txt-center w-32">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+    </div>,
 ];
