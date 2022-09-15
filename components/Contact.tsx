0f11d6817007f5f2707178e861f8c8b98b0e6881
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-15 23:42:57 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -131,28 +131,28 @@ const Contacts: React.FC<Props> = ({ submitDataFN }) => {
         </div>
       </div>
       <div className="h-full m-6">
-        <div className=" h-[50%] flex flex-col">
+        <div className=" flex flex-col">
         <div className="flex flex-row">
           <h4 className="text-6x1 flex-row">Instagram @cybernaut_official</h4>
-          <div className="flex flex-row mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-300"></div>
+          <div className="flex flex-row mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-blue-300 via-blue-500 to-blue-300"></div>
         </div>
-        <div className="flex flex-row gap-x-8">
+        <div className=" bg-blue-400 flex flex-row gap-x-20">
             <div><img src={temp.src}/></div>
             <div><img src={temp.src}/></div>
             <div><img src={temp.src}/></div>
             <div><img src={temp.src}/></div>
         </div>
         </div>
-        <div className=" h-[50%] flex flex-col">
+        <div className=" flex flex-col">
         <div className="flex flex-row ">
           <h4 className="text-6x1 flex-row">Instagram @cybernaut_official</h4>
-          <div className="flex flex-row pl-3 mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-300"></div>
+          <div className="flex flex-row pl-3 mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-red-300 via-red-500 to-red-300"></div>
         </div>
-        <div className="flex flex-row gap-x-3">
+        <div className="flex flex-row bg-red-400 gap-x-20">
             <div><img src={temp.src}/></div>
-            <div>hello</div>
-            <div>hello</div>
-            <div>hello</div>
+            <div><img src={temp.src}/></div>
+            <div><img src={temp.src}/></div>
+            <div><img src={temp.src}/></div>
         </div>
         </div>
       </div>
