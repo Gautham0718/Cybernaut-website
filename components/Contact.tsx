e8bf01d4dbb32d7646ab6100744b9c1358e2df0a
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-15 23:00:23 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -131,13 +131,19 @@ const Contacts: React.FC<Props> = ({ submitDataFN }) => {
         </div>
       </div>
       <div className="h-full m-6">
-        <div className="flex flex-row pr-3">
+        <div className="flex flex-row">
           <h4 className="text-6x1 flex-row">Instagram @cybernaut_official</h4>
-          <div className="flex flex-row mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-300"></div>
+          <div className="flex flex-row pl-3 mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-300"></div>
         </div>
-        <div className="flex flex-row pr-3">
+        <div className="flex flex-row">
+            <div>hello</div>
+            <div>hello</div>
+            <div>hello</div>
+            <div>hello</div>
+        </div>
+        <div className="flex flex-row ">
           <h4 className="text-6x1 flex-row">Instagram @cybernaut_official</h4>
-          <div className="flex flex-row mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-300"></div>
+          <div className="flex flex-row pl-3 mt-3 w-[50%] align-middle h-1 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-300"></div>
         </div>
       </div>
       <div className="h-full m-6 ">
