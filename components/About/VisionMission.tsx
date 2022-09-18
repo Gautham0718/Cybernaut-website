0d62be8617132ee60b2ff1afbf2597649a3c8bcb
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 20:16:00 +0530

    Update VisionMission.tsx

--- components/About/VisionMission.tsx
+++ components/About/VisionMission.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { CheckCircleIcon , KeyIcon} from "@heroicons/react/outline";
 
 const VisionMission = () => {
     return(
-        <div>Visiion sf middion</div>
+        <div id="background">
+            <div className="items-center content-center align-middle justify-center origin-center object-center">
+            <div className="absolute border-solid border border-gray-300  h-[400px] w-[400px] rounded-[600px] origin-top-left mt-20">
+                <CheckCircleIcon  className="absolute shadow-blue-200 shadow-sm ease-linear duration-200 hover:bg-blue-400 hover:text-white cursor-pointer py-0 w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-20"/>
+                <KeyIcon  className="absolute shadow-blue-200  hover:bg-blue-400 hover:text-white cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-44 -translate-y-4"/>
+              <div className=" absolute m-16 origin-center  shadow-gray-500  bg-gray-300 ease-linear duration-200 justify-center align-middle items-center border border-solid border-gray-300 w-64 h-64 rounded-full">
+                <p className=" justify-center ease-linear duration-200 pt-[25%] align-middle items-center text-2x1">Data Here</p>
+              </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
