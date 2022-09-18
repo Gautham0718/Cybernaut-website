604a2615144e39031a6381db8ef136990f7c744b
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 19:37:41 +0530

    Update about.tsx

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -5,23 +5,23 @@ import HomeLogo from "../../public/HomeLogo.jpeg";
 const about = () => {
     return(
         <div className="m-8">
-        <div className=" bg-gray-300 h-40 justify-center content-center items-center">
-            <blockquote className=" text-2xl">
+        <div className=" bg-gray-300 h-20 justify-center content-center items-center">
+            <div className=" text-2xl">
                 "Education is the most powerful weapon which you can use to change the world" - <span className="font-semibold">Nelson Mandela</span>
-            </blockquote>
+            </div>
         </div>
         <div className="flex flex-row">
             <div className="flex">
-                <img className=" relative flex float-left py-10 justify-center object-center align-middle" src = {HomeLogo.src}/>
+                <img className=" relative flex float-left  justify-center object-center align-middle" src = {HomeLogo.src}/>
             </div>
             <div className="flex flex-col w-fit pl-40">
             <h4 className="font-bold justify-left font-2xl">
                  So, why not provide that weapon to everyone?
             </h4>
-            <div className=" pt-4">
-                <div>This is the basic idea behind Cybernaut. Cybernaut is a registered startup company under MSME that has its vision of providing equal educational opportunities to everyone. </div>
-                <div>Knowledge is something that can be shared with someone, unlike money without any hesitation. Money is something fundamental for a living. Even though money is important, knowledge is the one that is the basic source for money. And so, education is very important. As an Ed-tech platform, we have our motto as 'Share your knowledge to expand others' knowledge.'</div>
-                <div>Cybernaut is a group of like-minded people who work towards the betterment of educational opportunities for everyone. In that manner, we conduct workshops and internships to impart knowledge to young minds.</div>
+            <div className=" pt-6">
+                <div className=" pt-6 justify-between">This is the basic idea behind Cybernaut. Cybernaut is a registered startup company under MSME that has its vision of providing equal educational opportunities to everyone. </div>
+                <div className=" pt-6 justify-between">Knowledge is something that can be shared with someone, unlike money without any hesitation. Money is something fundamental for a living. Even though money is important, knowledge is the one that is the basic source for money. And so, education is very important. As an Ed-tech platform, we have our motto as 'Share your knowledge to expand others' knowledge.'</div>
+                <div className=" pt-6 justify-between">Cybernaut is a group of like-minded people who work towards the betterment of educational opportunities for everyone. In that manner, we conduct workshops and internships to impart knowledge to young minds.</div>
             </div>
             </div>
         </div>
