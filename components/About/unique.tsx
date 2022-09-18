cbecab5988c24264244b61caa83ab322fa83bc9d
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 21:06:11 +0530

    Update unique.tsx

--- components/About/unique.tsx
+++ components/About/unique.tsx
@@ -10,7 +10,7 @@ const Unique = () =>{
                 <h1>How we are Unique</h1>
             </div>
             <div className="flex flex-row">
-                <div className="bg-gray-300 w-[50%] pr-3">
+                <div className="bg-gray-300 w-[50%]">
                     <div className="py-10">
                     Our uniqueness is our greatest strength. We ensure that no concept is memorized when it comes to our training, instead, they are made to understand right from the beginning i.e 'Smart Learning'.
                     </div>
@@ -18,14 +18,14 @@ const Unique = () =>{
                     Live classes and interaction during class support Smart Learning. Having quizzes during meets and seeing to it that all are in a comfortable environment with no pressure is what helps us stand out.
                     </div>
                 </div>
-                <div>
-                <div className=" align-bottom w-[20%] h-[20%] bg-gray-300 rounded-[200px] pr-3 ">
+                <div className="flex flex-row">
+                <div className=" align-bottom w-[20%] h-[20%] bg-gray-300 rounded-[500px] pl-4 ">
                     <img src = {juniorspace.src}/>
                 </div>
-                <div className=" align-bottom w-[40%] h-[40%] bg-gray-300 rounded-[400px] pr-3">
+                <div className=" align-bottom w-[40%] h-[40%] bg-gray-300 rounded-[800px] pl-4">
                     <img src = {HomeLogo.src}/>
                 </div>
-                <div className=" align-bottom w-[20%] h-[20%] bg-gray-300 rounded-[200px] ">
+                <div className=" align-bottom w-[20%] h-[20%] bg-gray-300 rounded-[500px] pl-4">
                     <img src = {StudentClub.src}/>
                 </div>
                 </div>
