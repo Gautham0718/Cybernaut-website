d71449bf3c22bbffaca76cd092a30ddd972eb16d
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 19:52:39 +0530

    Update about.tsx

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -4,15 +4,15 @@ import HomeLogo from "../../public/HomeLogo.jpeg";
 
 const about = () => {
     return(
-        <div className="m-8 w-full h-fit lg:h-[600px] md:w-4/5">
+        <div className="m-8">
         <div className=" bg-gray-300 h-20 justify-center content-center align-middle items-center">
-            <div className=" text-2xl align-middle justify-center items-center text-center object-center pt-10">
+            <div className=" text-2xl align-middle justify-center items-center text-center object-center pt-5">
                 "Education is the most powerful weapon which you can use to change the world" - <span className="font-semibold">Nelson Mandela</span>
             </div>
         </div>
         <div className="flex flex-row pt-10">
             <div className="flex items-center">
-                <img className=" relative flex justify-center object-center align-middle" src = {HomeLogo.src}/>
+                <img className=" relative flex justify-center object-center align-middle md:w-80" src = {HomeLogo.src}/>
             </div>
             <div className="flex flex-col w-fit pl-40">
             <h2 className="font-bold justify-left">
