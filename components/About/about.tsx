1fb30386c1942101180168bd2ae68d22eec9cde7
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 18:49:03 +0530

    Update about.tsx

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -12,13 +12,13 @@ const about = () => {
         </div>
         <div className="flex flex-row">
             <div className="flex">
-                <img className="flex float-left py-10 justify-center object-center align-middle" src = {HomeLogo.src}/>
+                <img className="flex float-left py-10 justify-center object-center w-[30%] h-[30%] align-middle" src = {HomeLogo.src}/>
             </div>
             <div className="flex flex-col w-fit gap-x-4">
             <h4 className="font-bold justify-left font-2xl">
                  So, why not provide that weapon to everyone?
             </h4>
-            <div className="gap-y-8">
+            <div className=" pt-4">
                 <div>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</div>
                 <div>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</div>
                 <div>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</div>
