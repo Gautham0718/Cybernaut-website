6fa83f662bab1bd5010b2486d458d578c98fafa6
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 19:46:53 +0530

    Update about.tsx

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -6,7 +6,7 @@ const about = () => {
     return(
         <div className="m-8">
         <div className=" bg-gray-300 h-20 justify-center content-center align-middle items-center">
-            <div className=" text-2xl align-middle justify-center items-center text-center object-center">
+            <div className=" text-2xl align-middle justify-center items-center text-center object-center pt-[50%]">
                 "Education is the most powerful weapon which you can use to change the world" - <span className="font-semibold">Nelson Mandela</span>
             </div>
         </div>
@@ -15,9 +15,9 @@ const about = () => {
                 <img className=" relative flex justify-center object-center align-middle" src = {HomeLogo.src}/>
             </div>
             <div className="flex flex-col w-fit pl-40">
-            <h4 className="font-bold justify-left">
+            <h2 className="font-bold justify-left">
                  So, why not provide that weapon to everyone?
-            </h4>
+            </h2>
             <div className=" pt-6">
                 <div className=" pt-6 justify-between">This is the basic idea behind Cybernaut. Cybernaut is a registered startup company under MSME that has its vision of providing equal educational opportunities to everyone. </div>
                 <div className=" pt-6 justify-between">Knowledge is something that can be shared with someone, unlike money without any hesitation. Money is something fundamental for a living. Even though money is important, knowledge is the one that is the basic source for money. And so, education is very important. As an Ed-tech platform, we have our motto as 'Share your knowledge to expand others' knowledge.'</div>
