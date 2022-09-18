3182a5d29ea99c65816c921a1f566a777c687403
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 20:25:30 +0530

    Update VisionMission.tsx

--- components/About/VisionMission.tsx
+++ components/About/VisionMission.tsx
@@ -3,12 +3,12 @@ import { CheckCircleIcon , KeyIcon} from "@heroicons/react/outline";
 
 const VisionMission = () => {
     return(
-        <div id="background" className="bg-white">
-            <div className="items-center content-center align-middle justify-center origin-center object-center">
-            <div className=" border-solid border border-gray-300  h-[400px] w-[400px] rounded-[600px] origin-top-left mt-20">
+        <div id="background" className="bg-white m-8">
+            <div className="items-center content-center align-middle justify-center origin-center  object-center p-[25%]">
+            <div className=" border-solid border border-gray-300  h-[50%] w-[50%] rounded-[600px] origin-top-left mt-20">
                 <CheckCircleIcon  className="absolute shadow-blue-200 shadow-sm ease-linear duration-200 hover:bg-blue-400 hover:text-white cursor-pointer py-0 w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-y-40 -translate-x-10"/>
                 <KeyIcon  className="absolute shadow-blue-200  hover:bg-blue-400 hover:text-white cursor-pointer w-12 h-12 border border-solid border-gray-400 rounded-full bg-white text-blue-400 translate-x-96 translate-y-40"/>
-              <div className=" absolute m-16 origin-center  shadow-gray-500  bg-gray-300 ease-linear duration-200 justify-center align-middle items-center border border-solid border-gray-300 w-64 h-64 rounded-full">
+              <div className=" absolute m-16 origin-center shadow  shadow-gray-500  bg-gray-300 ease-linear duration-200 justify-center align-middle items-center border border-solid border-gray-300 w-64 h-64 rounded-full">
                 <p className=" justify-center ease-linear duration-200 pt-[25%] align-middle items-center text-2x1">Data Here</p>
               </div>
                 </div>
