5921950557845ca204e2898f431ce0c235cef716
Prithiyanga <108605549+Prithiyanga@users.noreply.github.com>
2022-09-18 23:54:07 +0530

    Add files via upload

--- components/About/milestone.tsx
+++ components/About/milestone.tsx
@@ -3,33 +3,12 @@ import React, {useEffect, useState} from "react";
 
 
 
-const Milestone = (props: { size: number; progress: any; stroke: any; strokebg: any, stroke2: any }) =>{
-    const [offset, setOffset] = useState(0);
-    const { size, progress, stroke, strokebg, stroke2  } = props;
-    const mid = size / 2;
-    const radius = size / 2 - stroke / 2;
-    const circumference = 2 * Math.PI * radius;
-    useEffect(() =>{
-        const progressOff = ((100 - progress) / 100) * circumference;
-        setOffset(progressOff);
-    }, [setOffset, progress, circumference, offset])
+const Milestone = () =>{
+    
 
     return(
         <div>
-            <svg width = {size} height = {size} >
-                <circle stroke={strokebg} strokeWidth={stroke} cx = {mid} cy = {mid} r ={radius}  >
-                </circle>
-                <circle 
-                stroke={stroke2} 
-                strokeWidth={stroke} 
-                cx = {mid} 
-                cy = {mid} 
-                r ={radius} 
-                strokeDasharray= {circumference} 
-                strokeDashoffset={offset}>
-                </circle>
-                <text x={mid} y={mid}>{progress}+</text>
-            </svg>
+            MileStone Here
         </div>
     )
 };
